b18e19e3b14f05e4a0c62dc4fa703a7b60b37e08
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-15 15:14:55 +0200

    feat: saving data into google sheets

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 APP_ID = YOUR APP ID
 API_KEY = YOUR API KEY
@@ -9,6 +10,8 @@ HEIGHT_CM = YOUR HEIGHT
 AGE = YOUR AGE
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = YOUR SHEETY ENDPOINT
+
 input_text = input("Wich exercise did you do? ")
 
 headers = {"x-app-id": APP_ID, "x-app-key": APP_KEY, "x-remote-user": "0"}
@@ -19,4 +22,20 @@ parameters = {"query": input_text, "gender": GENDER, "weight_kg": WEIGHT_KG, "he
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
 
-print(result)
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
