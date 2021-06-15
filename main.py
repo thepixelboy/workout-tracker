6b6a7c94038192f1067ef1dfe19d13125d029b29
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-15 15:33:37 +0200

    feat: authenticate with sheety API and set environment variables

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+import os
 import requests
 from datetime import datetime
 
-APP_ID = YOUR APP ID
-API_KEY = YOUR API KEY
+APP_ID = os.environ.get("APP_ID")
+APP_KEY = os.environ.get("APP_KEY")
 
-GENDER = YOUR GENDER
-WEIGHT_KG = YOUR WEIGHT
-HEIGHT_CM = YOUR HEIGHT
-AGE = YOUR AGE
+GENDER = "Male"
+WEIGHT_KG = "80"
+HEIGHT_CM = "189"
+AGE = 45
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-sheet_endpoint = YOUR SHEETY ENDPOINT
+sheet_endpoint = f"https://api.sheety.co/{os.environ.get('SHEETY')}/workoutTracking/workouts"
 
 input_text = input("Wich exercise did you do? ")
 
@@ -18,12 +19,13 @@ headers = {"x-app-id": APP_ID, "x-app-key": APP_KEY, "x-remote-user": "0"}
 
 parameters = {"query": input_text, "gender": GENDER, "weight_kg": WEIGHT_KG, "height_cm": HEIGHT_CM, "age": AGE}
 
-
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
 
 today_date = datetime.now().strftime("%d/%m/%Y")
 now_time = datetime.now().strftime("%X")
+
+bearer_headers = {"Authorization": f"Bearer {os.environ.get('TOKEN')}"}
 
 for exercise in result["exercises"]:
     sheet_inputs = {
@@ -36,6 +38,6 @@ for exercise in result["exercises"]:
         }
     }
 
-    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs, headers=bearer_headers)
 
     print(sheet_response.text)
